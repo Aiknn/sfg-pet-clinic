b02cd939dcb76028243d86d535e60aec6db6c50d
Yerlan Nurlibayev <ynurlibayev@outlook.com>
2025-02-05 22:53:01 +0500

    POJO create. Closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+}
